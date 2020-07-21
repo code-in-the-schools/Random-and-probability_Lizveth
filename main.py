e0b722478754a822d682c469c638ab02e514e2e6
LizvethM <morales-garciaLizveth2021@email.com>
2020-07-21 15:37:16 +0000

    Random Generators 1,2,and 3_LIzvethM

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
 import random 
 #list for 10,100. and 1000
-one= 0 
-two= 0
-three= 0
-four= 0
-five= 0
-six= 0
-#Idk whats going on
-nums= ("1","2","3","4","5","6")
-numGen1 = random.randint(1,6)
-timesGen= 0
+
 listTen= []
+listHundred= []
+listHunThousand= []
+listData= [0, 0, 0, 0, 0, 0]
+
+#Idk whats going on
+nums= [1,2,3,4,5,6]
+nums2= [1,2,3,4,5,6]
+nums3= [1,2,3,4,5,6]
 #list and count variales 
 
-#while(timesGen!=10):
+######_____Num Gen 10
 for i in range(10):
-  listTen.append(numGen1)
+  listTen.append(random.randint(1,6))
   nums[listTen[i]-1]+=1
 
+for i in range(len(nums)):
+  print(str(i+1), "| ", "{:.2%}".format(nums[i]/10))
+print()
 
-  numGen1 = random.randint(1,6)
-  timesGen= timesGen+1
 
-  one+= 1
-  two+= 1
-  three+= 1
-  four+= 1
-  five+= 1
+#####______Num Gen 100
+for i in range(100):
+  listHundred.append(random.randint(1,6))
+  nums[listHundred[i]-1]+=1
 
-print(one)
-print(two)
-print(three)
-print(four)
-print(five)
+for i in range(len(nums)):
+  print(str(i+1), "| ", "{:.2%}".format(nums[i]/100))
+print()
+
+
+#####___Num Gen 100000
+
+for i in range(100000):
+  listHunThousand.append(random.randint(1,6))
+  nums[listHunThousand[i]-1]+=1
+
+for i in range(len(nums)):
+  print(str(i+1), "| ", "{:.2%}".format(nums[i]/100000))
+print()
 
